a975109d9bf3e68b53079a9daedfdc7a26506f81
waheddevs <abduvohid_gulomov@mail.ru>
2026-04-21 09:57:12 +0900

    feat: TASK-C has been done

--- train.js
+++ train.js
@@ -1,9 +1,38 @@
+/* C-TASK:
+Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
+MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
+*/
+
+
+// Masalaning yechimi:
+function checkContent(first, second) {
+  if(first.length !== second.length) return false;
+
+  let a = first.split('').sort().join('');
+  let b = second.split('').sort().join('');
+
+  return a === b;
+}
+
+console.log(checkContent('olma', 'almo'));
+console.log(checkContent('behi', 'bhe'));
+
+
+
+
+
+
+
+
+
+//==============================================================================================================================
+
 /* B-TASK:
 SAVOL: Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
 MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
 */
 
-
+/*
 // Masalaning yechimi:
 function countDigits(raqam) {
   let count = 0;
@@ -19,7 +48,7 @@ function countDigits(raqam) {
 console.log(countDigits('dfj5fwG6'));
 console.log(countDigits('dfj5f8wG6'));
 console.log(countDigits('dfj5fwG640Ghjv7'));
-
+*/
 
 
 
