7a2cebce477517f9e0720f60514297fb5c281080
waheddevs <abduvohid_gulomov@mail.ru>
2026-04-22 12:42:23 +0900

    feat: conditions and logical operators

--- opercon.py
+++ opercon.py
@@ -47,3 +47,58 @@ print('c is e', c is e)
 
 
 print('==== Condition ====')
+# Condition lar true hamda false ni emas, balki TRUTHY & FALTHY qiymatlarni tekshiradi
+
+# TRUTHY: True 100 -100 'MIT'
+# FALTHY: false 0 "" None
+
+x = 15
+
+if x > 50:
+    print('Case A')
+elif x > 10:             # elif = else if
+    print('Case B')
+else:
+    print('Case c')
+
+print('-------')
+
+
+print('==== Logical operators ====')
+
+age = 21
+'''
+person = None
+if age > 16:
+    person = 'adult'
+else:
+    person = 'child'
+
+'''
+
+# TERNARY = logical operators
+person = 'adult' if age > 18 else 'minor'
+print('person:', person)
+
+print('-------')
+
+is_student = True
+is_admin = False
+is_guest = True
+is_parent = True
+
+'''
+if is_student:
+    print('Executed')
+
+'''
+if not is_student:
+    print('Welcome here, do you want to be a student')
+elif is_admin:
+    print('please go to the office')
+elif is_guest or is_parent:          # or operatorida qatnashgan qiymatlardan hech bo'lmaganda bittasi True bo'lsa natija True bo'ladi
+    print('waiting room is over there')
+# elif is_guest and is_parent:          # and operatorida qatnashgan qiymatlardan hech bo'lmaganda bittasi False bo'lsa natija False bo'ladi
+#   print('waiting room is over there')
+else:
+    print('Etc')
