5e482128437e3f9f22aab8f90df6bc6e7604c478
waheddevs <abduvohid_gulomov@mail.ru>
2026-04-23 12:43:12 +0900

    feat: learnig unpacking arguments in tuple style

--- tuples.py
+++ tuples.py
@@ -30,5 +30,38 @@ animals = ('dog', 'cat', 'fish', 'lion')
 tuple_obj = ('MIT', 1000, True, None)
 
 # we can not change Tuple - ()
-print(animals[0])
-animals[0] = 'bird'
+# print(animals[0])
+# animals[0] = 'bird'
+
+# try avoid this (bu holatda qavslarsiz tuple hosil qilish xato emas, lekin bu uslub source imizni tushunarsiz holatga olib kelishi mumkin)
+# people = 'Andrew', 'John'
+
+print('===== Unpacking arguments =====')
+
+groups = ['MIT', 'FLEXY', 'DEVEX', 'MG']
+# Pythonda Bu guruhlarni argumentlarga yoyishda Tuple lar orqali amalga oshiriladi
+(x, y, z, a) = groups
+(x, y, *z) = groups  # (bu uslubda x hamda y birinchi va ikkinchi grouplarg yoyilsa, *z - undan keyingi kelgan barcha guruhlar z ga yoyilsin degan manoni anglatadi)
+print(f'the x: {x} and y: {y}')
+print('z:', z)  # list
+
+
+# * - args(argument > tuple)
+def calculate(*args):
+    print('*args >', *args)
+    total = 1
+    for x in args:
+        total *= x
+    print(f'the type(args) value: {type(args)}')
+    print(f'the total value: {total}')
+    return total
+
+
+# call
+calculate(1, 7, 2, 3)
+print('-------')
+calculate(0, 2, 300)
+print('-------')
+calculate(5, 7)
+
+# Qachonki argumentlarimiz soni noaniq bo'lsa, Tuple ga wrap qilib argumentlarni yoyishda ishlatamiz
