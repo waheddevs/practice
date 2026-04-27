7713d8fae5aada979097b62b1a4f109cd60c0b76
waheddevs <abduvohid_gulomov@mail.ru>
2026-04-27 14:22:17 +0900

    some changes

--- train.js
+++ train.js
@@ -1,4 +1,4 @@
-/* D-TASK
+/* E-TASK
 Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
 MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
 */
