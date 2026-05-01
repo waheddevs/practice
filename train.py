f6d0148933a5a1eb8b4f6ab45c0daa9d172c73ad
abdulwahed <146358092+waheddevs@users.noreply.github.com>
2026-05-01 09:54:39 +0900

    Remove unnecessary blank lines in train.py

--- train.py
+++ train.py
@@ -6,8 +6,6 @@ MASALAN: get_highest_index([5, 21, 12, 21, 8]) return qiladi 1 sonini.
 '''
 
 # Masalaning yechimi:
-
-
 def get_highest_index(arr):
     max_value = max(arr)
     first_index = arr.index(max_value)
