73b4bfd38f08ac5220226b7bf1d5585263c8e55e
waheddevs <abduvohid_gulomov@mail.ru>
2026-05-06 19:51:58 +0900

    I-Task has been done

--- train.py
+++ train.py
@@ -1,3 +1,25 @@
+''' I-TASK (PYTHON)
+
+Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+MASALAN: get_digits("m14i1t") return qiladi "141"
+
+'''
+# Masalaning yechimi:
+
+
+def get_digits(son):
+
+    result = ""
+    for char in son:
+        if char.isdigit():
+            result += char
+    return result
+
+
+print(get_digits("m14i1t"))
+print(get_digits("a1b2c3"))
+
+
 ''' G-TASK (PYTHON)
 
 Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
@@ -5,6 +27,7 @@ MASALAN: get_highest_index([5, 21, 12, 21, 8]) return qiladi 1 sonini.
 
 '''
 
+'''
 # Masalaning yechimi:
 def get_highest_index(arr):
     max_value = max(arr)
@@ -16,3 +39,5 @@ def get_highest_index(arr):
 print(get_highest_index([3, 5, 10, 10, 2]))
 print(get_highest_index([4, 9, 3]))
 print(get_highest_index([6, 8, 25, 11]))
+
+'''
