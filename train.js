e7c320677f2695818e05847e5afdc1a04f83f556
waheddevs <abduvohid_gulomov@mail.ru>
2026-05-18 15:25:13 +0900

    N-Task has been done

--- train.js
+++ train.js
@@ -1,9 +1,37 @@
+/* N-TASK (NodeJS)
+
+Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin.
+MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
+*/
+
+
+// Masalaning yechimi:
+function getSquareNumbers(numbers) {
+  const result = [];
+  for (let num of numbers) {
+    result.push({ number: num, square: num * num });
+  }
+  return result;
+}
+
+console.log(getSquareNumbers([8, 12, 5]));
+console.log(getSquareNumbers([7, 4, 13]));
+
+
+
+
+
+
+
+//==============================================================================================================================
+
 /* L-TASK (NodeJS)
 
 Shunday function yozing, u string qabul qilsin va string ichidagi hamma sozlarni chappasiga yozib va sozlar ketma-ketligini buzmasdan stringni qaytarsin.
 MASALAN: reverseSentence("we like coding!") return "ew ekil gnidoc";
 */
 
+/*
 // Masalaning yechimi:
 function reverseSentence(text) {
 
@@ -17,7 +45,7 @@ function reverseSentence(text) {
 
 console.log(reverseSentence("Aldaysan-ku Sardor"));
 console.log(reverseSentence("Nimo'lyapti?"));
-
+*/
 
 
 
